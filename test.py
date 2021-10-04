3a38a04c306a590a3957454e9de431a2ee47ad91
V31dy <71170989+V31dy@users.noreply.github.com>
2021-10-04 19:31:10 +0300

    Last add and compile game

--- test.py
+++ test.py
@@ -14,8 +14,27 @@ def test_moving_block():
     block.nextPos.row = 1
     assert block.nextPos.row == 1 and block.nextPos.col == 1
 
+def test_GameClock():
+    '''Тест проверки работы времени'''
+    clock = GameClock()
+    clock.update()
+    assert clock.frameTick == 1
 
+def test_MainBoard():
+    '''Тест проверки счета'''
+    board = MainBoard(blockSize, boardPosX, boardPosY, boardColNum, boardRowNum, boardLineWidth, blockLineWidth, scoreBoardWidth)
+    board.updateScores()
+    assert board.score == 0
 
+def test_MovingPiece():
+    key = GameKeyInput()
+    piece = MovingPiece(boardColNum, boardRowNum, 'uncreated')
+    board = MainBoard(blockSize, boardPosX, boardPosY, boardColNum, boardRowNum, boardLineWidth, blockLineWidth, scoreBoardWidth)
+    key.down.status = 'pressed'
+    key.xNav.status = 'right'
+    piece.movCollisionCheck('down')
+    piece.move(board.blockMat)
+    assert piece.status == 'moving'
 
 if __name__ == '__main__':
     pytest.main()
